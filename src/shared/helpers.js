ab936d2c7e4eddc3beb61343297636d0294dff0d
Alchamyst <ashley@ashley-yetman.co.uk>
2023-09-18 19:15:52 +0100

    add helpers for attack, magic and heal calcualtions.

--- src/shared/helpers.js
+++ src/shared/helpers.js
@@ -3,3 +3,21 @@ export const wait = ( milliseconds ) => new Promise(resolve => {
         resolve();
     }, milliseconds)
 });
+
+export const attack = ({ attacker, receiver }) => {
+    const recievedDamage = attacker.attack - (attacker.level - receiver.level) * 1.25;  
+    const finalDamage = recievedDamage - receiver.defense / 2;
+
+    return finalDamage;
+};
+
+export const magic = ({ attacker, receiver }) => {
+    const recievedDamage = attacker.magic - (attacker.level - receiver.level) * 1.25; 
+    const finalDamage = recievedDamage - receiver.magicdefense / 2;
+
+    return finalDamage;
+};
+
+export const heal = ({ reciever }) => {
+    return reciever.magic + reciever.level * 0.25;
+}
